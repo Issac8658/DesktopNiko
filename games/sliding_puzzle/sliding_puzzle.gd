68b33d4ddb0640b15d062a06e0ecbb79877c0f19
Issac332 <kirillanis07200@gmail.com>
2026-04-02 15:21:09 +0300

    some sliding game mechanic
    i do this commit on my linux laptop, i think something can be broken, just testing

--- games/sliding_puzzle/sliding_puzzle.gd
+++ games/sliding_puzzle/sliding_puzzle.gd
@@ -1,6 +1,7 @@
 extends Control
 
 const CELLS_COUNT := Vector2i(4, 4)
+const ANIM_TIME : float = 2
 
 @export var part_template : PackedScene
 @export var parts_gap : int;
@@ -10,19 +11,52 @@ func _ready() -> void:
 	for y in range(CELLS_COUNT.y):
 		for x in range(CELLS_COUNT.x):
 			if Vector2i(x + 1, y + 1) != CELLS_COUNT:
+				var pos := Vector2i(x, y)
 				var part : Control = part_template.instantiate()
 				add_child(part)
 				part.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
-				update_part(part, Vector2i(x, y))
+				update_part(part, pos)
 				part.text = str(y * CELLS_COUNT.x + x + 1)
-				parts[Vector2i(x,y)] = part
+				parts[pos] = part
+				
+				part.gui_input.connect(func (event):
+					if (event is InputEventMouseButton):
+						if (event.pressed and event.button_index == MOUSE_BUTTON_LEFT):
+							var curr_pos : Vector2i = parts.find_key(part)
+							move_part(curr_pos)
+							#print(curr_pos)
+				)
+				part.mouse_entered.connect(func():
+					if (DisplayServer.mouse_get_button_state() == MOUSE_BUTTON_MASK_LEFT):
+						var curr_pos : Vector2i = parts.find_key(part)
+						move_part(curr_pos)
+						print(curr_pos)
+				)
 
 func move_part(part_pos: Vector2i):
-	if (not parts.has(part_pos - Vector2i(0, 1))):
-			pass
+	var part = parts.get(part_pos)
+	
+	if (part != null):
+		if (part_pos.y > 0 and not parts.has(part_pos - Vector2i(0, 1))):
+				parts.erase(part_pos)
+				parts.set(part_pos - Vector2i(0, 1), part)
+				smooth_update_part(part, part_pos - Vector2i(0, 1), part_pos)
+		elif (part_pos.y < CELLS_COUNT.y - 1 and not parts.has(part_pos + Vector2i(0, 1))):
+				parts.erase(part_pos)
+				parts.set(part_pos + Vector2i(0, 1), part)
+				smooth_update_part(part, part_pos + Vector2i(0, 1), part_pos)
+		elif (part_pos.x > 0 and not parts.has(part_pos - Vector2i(1, 0))):
+				parts.erase(part_pos)
+				parts.set(part_pos - Vector2i(1, 0), part)
+				smooth_update_part(part, part_pos - Vector2i(1, 0), part_pos)
+		elif (part_pos.x < CELLS_COUNT.x - 1 and not parts.has(part_pos + Vector2i(1, 0))):
+				parts.erase(part_pos)
+				parts.set(part_pos + Vector2i(1, 0), part)
+				smooth_update_part(part, part_pos + Vector2i(1, 0), part_pos)
 
 
 func update_part(part : Control, pos : Vector2i):
+	part.target_pos = Rect2(float(pos.x) / float(CELLS_COUNT.x), float(pos.y) / float(CELLS_COUNT.y), float(pos.x + 1) / float(CELLS_COUNT.x), float(pos.y + 1) / float(CELLS_COUNT.y))
 	@warning_ignore("integer_division")
 	part.set_anchor_and_offset(SIDE_TOP, float(pos.y) / float(CELLS_COUNT.y), parts_gap / 2)
 	@warning_ignore("integer_division")
@@ -31,3 +65,32 @@ func update_part(part : Control, pos : Vector2i):
 	part.set_anchor_and_offset(SIDE_LEFT, float(pos.x) / float(CELLS_COUNT.x), parts_gap / 2)
 	@warning_ignore("integer_division")
 	part.set_anchor_and_offset(SIDE_RIGHT, float(pos.x + 1) / float(CELLS_COUNT.x), -parts_gap / 2)
+
+func smooth_update_part(part : Control, pos : Vector2i, old_pos : Vector2i):
+	part.target_pos = Rect2(float(pos.x) / float(CELLS_COUNT.x), float(pos.y) / float(CELLS_COUNT.y), float(pos.x + 1) / float(CELLS_COUNT.x), float(pos.y + 1) / float(CELLS_COUNT.y))
+	#if (old_pos.y != pos.y):
+	#	var t1 = part.create_tween()
+	#	var t2 = part.create_tween()
+	#	t1.set_trans(Tween.TRANS_ELASTIC)
+	#	t2.set_trans(Tween.TRANS_ELASTIC)
+	#	t1.set_ease(Tween.EASE_OUT)
+	#	t2.set_ease(Tween.EASE_OUT)
+	#	if (old_pos.y > pos.y):
+	#		t1.tween_property(part, "anchor_top", float(pos.y) / float(CELLS_COUNT.y), ANIM_TIME)
+	#		t2.tween_property(part, "anchor_bottom", float(pos.y + 1) / float(CELLS_COUNT.y), ANIM_TIME + 0.2)
+	#	else:
+	#		t1.tween_property(part, "anchor_bottom", float(pos.y + 1) / float(CELLS_COUNT.y), ANIM_TIME)
+	#		t2.tween_property(part, "anchor_top", float(pos.y) / float(CELLS_COUNT.y), ANIM_TIME + 0.2)
+	#if (old_pos.x != pos.x):
+	#	var t1 = part.create_tween()
+	#	var t2 = part.create_tween()
+	#	t1.set_trans(Tween.TRANS_ELASTIC)
+	#	t2.set_trans(Tween.TRANS_ELASTIC)
+	#	t1.set_ease(Tween.EASE_OUT)
+	#	t2.set_ease(Tween.EASE_OUT)
+	#	if (old_pos.x > pos.x):
+	#		t1.tween_property(part, "anchor_left", float(pos.x) / float(CELLS_COUNT.x), ANIM_TIME)
+	#		t2.tween_property(part, "anchor_right", float(pos.x + 1) / float(CELLS_COUNT.x), ANIM_TIME + 0.2)
+	#	elif (old_pos.x != pos.x):
+	#		t1.tween_property(part, "anchor_right", float(pos.x + 1) / float(CELLS_COUNT.x), ANIM_TIME)
+	#		t2.tween_property(part, "anchor_left", float(pos.x) / float(CELLS_COUNT.x), ANIM_TIME + 0.2)
--- scripts/windows/window_controller.gd
+++ scripts/windows/window_controller.gd
@@ -7,21 +7,21 @@ signal close_button_pressed();
 @export var hide_button : BaseButton
 @export var ignore_on_top_settings : bool = false;
 
-var is_dragging : bool = false;
-var mouse_offset : Vector2;
+#var is_dragging : bool = false;
+#var mouse_offset : Vector2;
 
 func _ready() -> void:
 	topbar.gui_input.connect(func (event : InputEvent):
 		if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 			if event.is_pressed():
 				if get_visible_rect().has_point(event.position):
-					#DisplayServer.window_start_drag(get_window_id())
-					is_dragging = true
-					mouse_offset = event.position
-			else:
-				is_dragging = false
-		if event is InputEventMouseMotion and is_dragging:
-			MoveWindow(event.position - mouse_offset)
+					DisplayServer.window_start_drag(get_window_id())
+		#			is_dragging = true
+		#			mouse_offset = event.position
+		#	else:
+		#		is_dragging = false
+		#if event is InputEventMouseMotion and is_dragging:
+		#	MoveWindow(event.position - mouse_offset)
 	)
 	
 	close_button.pressed.connect(func ():
@@ -47,6 +47,7 @@ func _ready() -> void:
 	if not ignore_on_top_settings:
 		always_on_top = ValuesContainer.WindowsAlwaysOnTop;
 		Update()
+
 func MoveWindow(Offset:Vector2i) -> void:
 	position += Vector2i(Offset);
 
