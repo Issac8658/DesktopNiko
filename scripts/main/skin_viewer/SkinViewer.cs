a2832a8a1b8fb7010f553ebe277491c427337476
Issac332 <119879937+Issac8658@users.noreply.github.com>
2026-06-07 23:59:10 +0300

    idk

--- scripts/main/skin_viewer/SkinViewer.cs
+++ scripts/main/skin_viewer/SkinViewer.cs
@@ -9,18 +9,25 @@ public partial class SkinViewer : Window
 	[Export] public Node2D LeftBorder;
 	public override void _Ready()
 	{
-		Vector2I SPos = DisplayServer.ScreenGetPosition(DisplayServer.GetPrimaryScreen());
-		Vector2I SSize = DisplayServer.ScreenGetSize(DisplayServer.GetPrimaryScreen());
-		Rect2I SFree = DisplayServer.ScreenGetUsableRect(DisplayServer.GetPrimaryScreen());
-		Position = SPos;
-		Size = SSize - new Vector2I(0, 2);
-
-		TopBorder.Position = new (TopBorder.Position.X, SFree.Position.Y - Size.Y / 2);
-		BottomBorder.Position = new (BottomBorder.Position.X, SFree.Position.Y + SFree.Size.Y - Size.Y / 2);
-		LeftBorder.Position = new (SFree.Position.X - Size.X / 2, LeftBorder.Position.Y);
-		RightBorder.Position = new (SFree.Position.X + SFree.Size.X - Size.X / 2, RightBorder.Position.Y);
-
 		ExitButton.Pressed += Hide;
 		CloseRequested += Hide;
+		VisibilityChanged += Update;
+
+		Update();
+	}
+
+	private void Update()
+	{
+		int Screen = DisplayServer.WindowGetCurrentScreen(GetWindowId());
+		Vector2I ScreenPos = DisplayServer.ScreenGetPosition(Screen);
+		Vector2I ScreenSize = DisplayServer.ScreenGetSize(Screen);
+		Rect2I ScreenFreeArea = DisplayServer.ScreenGetUsableRect(Screen);
+		Position = ScreenPos;
+		Size = ScreenSize - new Vector2I(0, 2);
+
+		TopBorder.Position = new (TopBorder.Position.X, ScreenFreeArea.Position.Y - Size.Y / 2);
+		BottomBorder.Position = new (BottomBorder.Position.X, ScreenFreeArea.Position.Y + ScreenFreeArea.Size.Y - Size.Y / 2);
+		LeftBorder.Position = new (ScreenFreeArea.Position.X - Size.X / 2, LeftBorder.Position.Y);
+		RightBorder.Position = new (ScreenFreeArea.Position.X + ScreenFreeArea.Size.X - Size.X / 2, RightBorder.Position.Y);
 	}
 }
